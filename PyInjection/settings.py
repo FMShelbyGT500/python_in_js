f4db10b068f920cc71e9a9adf700a7ee23e3ff19
Vlad Novosadenko <novosadenko.v23@gmail.com>
2018-04-18 04:13:32 +0300

    pyinjs_0.1.1

--- PyInjection/settings.py
+++ PyInjection/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = '!es2(+bnt3x$yc&%v@j6!jg9)j8n@f($op-(%l91ac1u5jse^a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['127.0.0.1', 'python-in-js.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'python-in-js.herokuapp.com']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
